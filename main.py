3f8cfea1dd7a7444b163534f16a7c5518a5a7de6
riyapuri2005 <riyapuri0710@gmail.com>
2024-04-11 20:39:06 +0530

    \\ Fixed form receiving None as a response when viewer Disconnects. \\ Created variables for all parameters

--- main.py
+++ main.py
@@ -1,21 +1,31 @@
 from gevent import monkey
+
 monkey.patch_all()
 
 from gevent.pywsgi import WSGIServer
 from internal.apps import *
 
 
-def process_form(viewerObj:BaseViewer, form:dict):
-    print(viewerObj.viewerID, form)
+def process_form(viewerObj: BaseViewer, form: dict):
+    if form is not None:
+        print(viewerObj.viewerID, form)
+    else:
+        print("Disconnected: ", viewerObj.viewerID)
 
-def newVisitor(viewerObj:BaseViewer):
+
+def newVisitor(viewerObj: BaseViewer):
     print("New Viewer: ", viewerObj.viewerID)
-    HTMLtoSend = ""
+    HTMLtoSend = "helo"
     viewerObj.queueTurboAction(HTMLtoSend, "mainDiv", viewerObj.turboApp.methods.update)
 
 
-baseApp, turboApp = createApps(process_form, newVisitor, "Dynamic Website", "/", "/ws", 'GNwHvssnLQVKYPZk0D_Amy9m3EeSvi6Y1FiHfTO8F48=', ["login", "register"], "")
+fernetKey = 'GNwHvssnLQVKYPZk0D_Amy9m3EeSvi6Y1FiHfTO8F48='
+appName = "Dynamic Website"
+homePageRoute = "/demo"
+WSRoute = f"{homePageRoute}_ws"
+purposes = ["login", "register"]
+extraHeads = """ """
+baseApp, turboApp = createApps(process_form, newVisitor, appName, homePageRoute, WSRoute, fernetKey, purposes, extraHeads)
 
-
-print(f"http://127.0.0.1:49000")
+print(f"http://127.0.0.1:49000{homePageRoute}")
 WSGIServer(('0.0.0.0', 49000,), baseApp, log=None).serve_forever()
